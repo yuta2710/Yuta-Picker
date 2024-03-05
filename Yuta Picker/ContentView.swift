0485a9d786a3334ce41ee690f3e3aa16b257de3f
Nguyen Phuc Loi <digivantrix0802.ent@gmail.com>
2024-03-05 15:12:19 +0700

    Initial Commit

---
+++ Yuta Picker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Yuta Picker
+//
+//  Created by Nguyen Phuc Loi on 05/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Yuta Picker/Yuta_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yuta_PickerApp.swift
+//  Yuta Picker
+//
+//  Created by Nguyen Phuc Loi on 05/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Yuta_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
